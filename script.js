13e11577853df25886c85cac75f265dadfa9356b
prakashboss <b.prakashboss@gmail.com>
2020-05-17 19:10:04 +1200

    Adding images of projecs and skills
    Adding grid layout to the skills and images and links to skills and projects

---
+++ script.js
@@ -0,0 +1,33 @@
+// const body = document.querySelector("body");
+
+// const navBarUl = document.querySelector(".navBar");
+// var navBarLi = document.querySelectorAll(".navBar  li a");
+// const screenWidth = screen.availWidth;
+// const screenHeight = screen.width;
+
+
+
+// console.log(navBarLi);
+
+// // const start = new TimelineMax();
+
+// // start.fromTo(navBarLi, 3, {background:"white"}, {background: "black"} );
+
+
+
+// // // animation for screen size under 500px
+// // if(screenWidth<500) {
+// //     navBarUl.innerHTML= "<img src=\"./pictures/nav.png\" height=\"40\" width=\"50\" />";
+
+
+// //     // navBarUl.style.background="linear-gradient(to left, rgba(255, 255, 255, 0.938),lightblue";
+// //     // for(let i =0; i<6;i++){
+// //     //     navBarLi[i].innerHTML= "<img scr>";
+// //     //     // navBarLi[i].style.z-index = "absolute";
+// //     //     // console.log(navBarLi[i]);
+// }
+
+
+    
+
+// // }
